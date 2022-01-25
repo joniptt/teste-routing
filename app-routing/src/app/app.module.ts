0e5a12fe7e7771be7009d51c0811da2196a5fa37
João Antonio Soares dos Passos <jdospassos8@gmail.com>
2022-01-24 21:21:57 -0300

    Template structure

--- app-routing/src/app/app.module.ts
+++ app-routing/src/app/app.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EditComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    ServersComponent,
+    HomeComponent,
+    EditComponent,
+    ServerComponent,
+  ],
+  imports: [BrowserModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ app-routing/src/app/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit-server.component.html',
+  styleUrls: ['./edit-server.component.css'],
+})
+export class EditComponent {}
